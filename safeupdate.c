95d160e755ac57eb5a0c491d7707103348c3f9c5
Eric Radman <ericshane@eradman.com>
2021-06-02 13:31:34 -0400

    Adapt build for PostgreSQL 14

--- safeupdate.c
+++ safeupdate.c
@@ -11,6 +11,50 @@ bool safeupdate_enabled;
 
 static post_parse_analyze_hook_type prev_post_parse_analyze_hook = NULL;
 
+#if (PG_VERSION_NUM >= 140000)
+static void
+delete_needs_where_check(ParseState *pstate, Query *query, JumbleState *jstate)
+{
+	ListCell *l;
+	Query *ctequery;
+
+	if (!safeupdate_enabled)
+		return;
+
+	if (query->hasModifyingCTE) {
+		foreach(l, query->cteList)
+		{
+			CommonTableExpr *cte = (CommonTableExpr *) lfirst(l);
+			ctequery = castNode(Query, cte->ctequery);
+			delete_needs_where_check(pstate, ctequery, jstate);
+		}
+	}
+
+	switch (query->commandType)
+	{
+	case CMD_DELETE:
+	if (query->commandType == CMD_DELETE)
+	{
+		Assert(query->jointree != NULL);
+		if (query->jointree->quals == NULL)
+			ereport(ERROR,
+			    (errcode(ERRCODE_CARDINALITY_VIOLATION),
+			     errmsg("DELETE requires a WHERE clause")));
+	}
+		break;
+	case CMD_UPDATE:
+		Assert(query->jointree != NULL);
+		if (query->jointree->quals == NULL)
+			ereport(ERROR,
+			    (errcode(ERRCODE_CARDINALITY_VIOLATION),
+			     errmsg("UPDATE requires a WHERE clause")));
+	default:
+		break;
+	}
+	if (prev_post_parse_analyze_hook != NULL)
+		(*prev_post_parse_analyze_hook) (pstate, query, jstate);
+}
+#else
 static void
 delete_needs_where_check(ParseState *pstate, Query *query)
 {
@@ -53,6 +97,7 @@ delete_needs_where_check(ParseState *pstate, Query *query)
 	if (prev_post_parse_analyze_hook != NULL)
 		(*prev_post_parse_analyze_hook) (pstate, query);
 }
+#endif
 
 void
 _PG_init(void)
